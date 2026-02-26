3a8de27cb6c9318223030aea36f3be731791c0d4
kaiquejw <kaiquecapt@gmail.com>
2026-02-26 10:23:49 -0300

    Adicionando janaine para os testes

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ CONTAS = [
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 10
-MINUTO_ALVO = 23
+MINUTO_ALVO = 26
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
