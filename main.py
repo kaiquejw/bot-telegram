496698023aa0e078ef20f7a09aa9770e0ac205d0
kaiquejw <kaiquecapt@gmail.com>
2026-02-26 11:20:35 -0300

    Adicionando ANA para os testes

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ CONTAS = [
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 11
-MINUTO_ALVO = 19
+MINUTO_ALVO = 22
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
