a96a4b029b254ff1dd88fbcc498d2a3ce2fe04c3
kaiquejw <kaiquecapt@gmail.com>
2026-03-05 20:08:19 -0300

    PROD pegar senha mih

--- main.py
+++ main.py
@@ -16,21 +16,15 @@ CONTAS = [
 
 
 
-#      20h
-{
-        "nome": "ana", 
-        "secret_name": "SESSION_ANA",
-        "chat_id": -1002443109385,
-        "msg": "Ana x eduardo raio 4"
-    },
+
 
     #      20h30
-  #  {
-  #      "nome": "Michele",
-  #      "secret_name": "SESSION_MICHELE",
-  #      "chat_id": -1002704903786,  
-  #      "msg": "Michele x Paquistão raio 3"
-  #  },
+    {
+        "nome": "Michele",
+        "secret_name": "SESSION_MICHELE",
+        "chat_id": -1002704903786,  
+        "msg": "Michele x Paquistão raio 3"
+    },
 
   #      21h00
  
@@ -45,7 +39,7 @@ CONTAS = [
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 20
-MINUTO_ALVO = 00
+MINUTO_ALVO = 30
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
